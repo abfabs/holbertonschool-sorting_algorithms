afdb1ab025a37c7391545609fd62f764587adf56
abfabs <albaeftimi@gmail.com>
2025-08-05 10:50:57 +0000

    quick sorting

--- sort.h
+++ sort.h
@@ -5,16 +5,15 @@
 
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Function prototypes */
@@ -23,7 +22,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void lomuto_sorting(int *array, int min_index, int pivot_index, size_t size);
 int arrangement(int *array, int min_index, int pivot_index, size_t size);
